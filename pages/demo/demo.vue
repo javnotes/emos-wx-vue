84300cbd86aa4ab93f703c15415f10f424613823
vuffy <monkeyd.vuffy@gmail.com>
2021-11-08 23:56:29 +0800

    init

---
+++ pages/demo/demo.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<view>{{username}}</view>
+		<view v-for="one in tel">{{one}}</view>
+		<view v-if="age>=18">
+			<button @tap="signUp()">报名按钮</button>
+		</view>
+		<view>
+			<input type="text" v-model="address" placeholder="输入地址信息" />
+		</view>
+		<view>{{address}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				username: "Scott",
+				tel: ["13312345678", "15912345678"],
+				age: '20',
+				address:"shanghai"
+			}
+		},
+		methods: {
+			signUp: function() {
+				uni.showToast({
+					title: "用户点击了报名按钮"
+				})
+			}
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
