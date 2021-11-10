a95d75e360bfb9d8d620bc1e84bb8c4718baedc4
vuffy <monkeyd.vuffy@gmail.com>
2021-11-10 13:18:57 +0800

    modified register.vue

--- pages/register/register.vue
+++ pages/register/register.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
-		
+		<image src="../../static/logo-2.png" mode="widthFix" class="logo"></image>
+		<view class="register-container">
+			<input placeholder="输入你的邀请码" class="register-code" maxlength="6" v-model="registerCode" />
+			<view class="register-desc">管理员创建员工证账号之后，你可以从你的个人邮箱中获得注册邀请码</view>
+			<button class="register-btn" open-type="getUserInfo" @tap="register()">执行注册</button>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +13,19 @@
 	export default {
 		data() {
 			return {
-				
-			}
+				registerCode: ''
+			};
 		},
 		methods: {
-			
+			toRegister: function() {
+				uni.navigateTo({
+					url: '../register/register'
+				});
+			}
 		}
-	}
+	};
 </script>
 
-<style>
-
+<style lang="less">
+	@import url('register.less');
 </style>
